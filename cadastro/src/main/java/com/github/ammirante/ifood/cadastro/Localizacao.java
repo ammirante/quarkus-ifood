06c9158e93e0514e808af99945a9ed124259f8a1
douglas.cunha <douglas.cunha@fornecedores.sicoob.com.br>
2020-08-24 17:40:11 -0300

    Criação do docker-compose (postegres), projeto inicial quarkus com um CRUD dos restaurantes

---
+++ cadastro/src/main/java/com/github/ammirante/ifood/cadastro/Localizacao.java
@@ -0,0 +1,26 @@
+package com.github.ammirante.ifood.cadastro;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * 
+ * Localizacao
+ *
+ */
+@Entity
+@Table(name = "localizacao")
+public class Localizacao {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	public Long id;
+	
+	public Double latitute;
+	
+	public Double longitude;
+	
+}
---
+++ cadastro/src/main/java/com/github/ammirante/ifood/cadastro/Prato.java
@@ -0,0 +1,35 @@
+package com.github.ammirante.ifood.cadastro;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
+
+/**
+ * 
+ * Prato
+ *
+ */
+@Entity
+@Table(name = "prato")
+public class Prato extends PanacheEntityBase {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	public Long id;
+	
+	public String nome;
+	
+	public String descricao;
+	
+	@ManyToOne
+	public Restaurante restaurante;
+	
+	public BigDecimal preco;
+}
